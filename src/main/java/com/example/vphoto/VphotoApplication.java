9424c8447cbe506fb797f6aa7a5943eb7afcf8a0
rahmonali96 <rahmonaliyoqubov@gmail.com>
2022-12-10 02:40:08 +0500

    2022-12-10-1 Added test environment

--- src/main/java/com/example/vphoto/VphotoApplication.java
+++ src/main/java/com/example/vphoto/VphotoApplication.java
@@ -2,8 +2,10 @@ package com.example.vphoto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("dev")
 public class VphotoApplication {
 
     public static void main(String[] args) {
